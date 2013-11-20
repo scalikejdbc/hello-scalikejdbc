ae04d2757ccf68f329d1b2a7873beb961efd4da5
Kazuhiro Sera <seratch@gmail.com>
2013-11-20 14:44:41 +0900

    Applied scalikejdbc 1.7

--- project/Build.scala
+++ project/Build.scala
@@ -3,19 +3,19 @@ import play.Project._
 
 object DevTeamBuild extends Build {
 
-  lazy val scalikejdbcVersion = "1.6.5"
+  lazy val scalikejdbcVersion = "1.7.0"
   lazy val h2Version = "1.3.172"
 
   lazy val app = {
     val appName         = "hello-scalikejdbc"
     val appVersion      = "0.1"
     val appDependencies = Seq(
-      "com.github.seratch"   %% "scalikejdbc"                     % scalikejdbcVersion,
-      "com.github.seratch"   %% "scalikejdbc-config"              % scalikejdbcVersion,
-      "com.github.seratch"   %% "scalikejdbc-interpolation"       % scalikejdbcVersion,
-      "com.github.seratch"   %% "scalikejdbc-play-plugin"         % scalikejdbcVersion,
-      "com.github.seratch"   %% "scalikejdbc-play-fixture-plugin" % scalikejdbcVersion,
-      "com.h2database"       %  "h2"                              % "1.3.172",
+      "org.scalikejdbc"      %% "scalikejdbc"                     % scalikejdbcVersion,
+      "org.scalikejdbc"      %% "scalikejdbc-config"              % scalikejdbcVersion,
+      "org.scalikejdbc"      %% "scalikejdbc-interpolation"       % scalikejdbcVersion,
+      "org.scalikejdbc"      %% "scalikejdbc-play-plugin"         % scalikejdbcVersion,
+      "org.scalikejdbc"      %% "scalikejdbc-play-fixture-plugin" % scalikejdbcVersion,
+      "com.h2database"       %  "h2"                              % "1.3.174",
       "org.hibernate"        %  "hibernate-core"                  % "4.2.3.Final",
       "org.json4s"           %% "json4s-ext"                      % "3.2.4",
       "com.github.tototoshi" %% "play-json4s-native"              % "0.1.0",
@@ -25,10 +25,13 @@ object DevTeamBuild extends Build {
       "org.webjars"          %  "backbonejs"                      % "1.0.0-1",
       "org.webjars"          %  "underscorejs"                    % "1.5.1",
       "org.webjars"          %  "bootstrap"                       % "2.3.2",
-      "com.github.seratch"   %% "scalikejdbc-test"                % scalikejdbcVersion  % "test",
+      "org.scalikejdbc"      %% "scalikejdbc-test"                % scalikejdbcVersion  % "test",
       "org.specs2"           %% "specs2"                          % "2.1"               % "test"
     )
-    play.Project(appName, appVersion, appDependencies).settings(scalaVersion in ThisBuild := "2.10.2")
+    play.Project(appName, appVersion, appDependencies).settings(
+      scalaVersion in ThisBuild := "2.10.3",
+      conflictWarning := ConflictWarning.disable
+    )
   }
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,12 +1,15 @@
+resolvers ++= Seq(
+  "sonatype releases" at "http://oss.sonatype.org/content/repositories/releases",
+  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+)
+
 libraryDependencies += "com.h2database" % "h2" % "[1.3,)"
 
-addSbtPlugin("com.github.seratch" %% "scalikejdbc-mapper-generator" % "1.6.5")
-
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.1")
-
-resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+addSbtPlugin("org.scalikejdbc" %% "scalikejdbc-mapper-generator" % "1.7.0")
 
 logLevel := Level.Warn
 
-addSbtPlugin("play" % "sbt-plugin" % "2.1.2")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.1")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "[0.1,)")
 
